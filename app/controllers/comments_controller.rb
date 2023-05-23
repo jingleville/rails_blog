b9549e18f30b9626efd5a60c3d169378c1cdaf5a
jingleville <andrewgavrick@yandex.ru>
2023-05-23 18:13:53 +0300

    hide comments form until user is signed in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
+	before_action :authenticate_user!, only: %i[create]
+
 	def create
 		@article = Article.find(params[:article_id])
-		@comment = @article.comments.create comment_params
+		@comment = @article.comments.new
+		@comment.body = comment_params[:body]
+		@comment.author = current_user.username
+
+		@comment.save
 
 		redirect_to articles_path(@article)
 	end
@@ -10,7 +16,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit(:body)
   end
 
 
