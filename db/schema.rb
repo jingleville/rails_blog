88a49b1edb1b960df13d22bfd4f106d7452df442
jingleville <andrewgavrick@yandex.ru>
2023-05-23 17:23:30 +0300

    added username column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_115354) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_142224) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -54,8 +54,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_115354) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
