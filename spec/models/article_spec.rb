a4c55d47a154035e7e6233147e9b6e52759da0bc
jingleville <andrewgavrick@yandex.ru>
2023-05-24 14:16:54 +0300

    updated article test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,23 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  text       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "rails_helper"
 
 describe Article do
-	it { should have_many :comments }
+	describe 'validations' do
+		it { should validate_presence_of :title }
+		it { should validate_presence_of :text }
+	end
+
+	describe 'associations' do
+		it { should have_many :comments }
+	end
+	
 end
