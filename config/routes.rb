a422cfa39dfb2e8cf54511e3e9ab3e5fe61b3ec2
jingleville <andrewgavrick@yandex.ru>
2023-05-24 12:07:14 +0300

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'home#index'
+
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root 'home#index'
-
-  
-  get 'contacts', to: 'contacts#new'
-  post 'contacts', to: 'contacts#create'
-  
-
-  get 'terms' => 'pages#terms'
-  get 'about' => 'pages#about'
 
   resources :articles do
     resources :comments, only: %i[create]
   end
+
+  resource :contacts, only: %i[new create]
+
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
